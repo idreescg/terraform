279250edd885322b933b5f12e39722137b9fcc99
idreescg <idreescg@gmail.com>
2023-02-18 19:34:40 +0530

    addition of ec2 instance

--- vpc.tf
+++ vpc.tf
@@ -70,3 +70,54 @@ resource "aws_route_table_association" "associate_private_rt" {
 }
 
 #--------------------------------------------End of Private Subnet Block-------------------------------------
+
+#--------------------------------------------Creation of ec2 instance with public Subnet---------------------
+resource "aws_instance" "web_instance" {
+  ami           = "ami-0f1a5f5ada0e7da53"
+  instance_type = "t2.micro"
+  key_name      = "Myec2Keypair"
+
+  subnet_id                   = aws_subnet.public_subnet_us-west-2a.id
+  vpc_security_group_ids      = [aws_security_group.sg_webserver.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+  #!/bin/bash -ex
+
+  amazon-linux-extras install nginx1 -y
+  systemctl enable nginx
+  systemctl start nginx
+  EOF
+
+  tags = {
+    "Name" : "MyterraformEc2"
+  }
+}
+
+
+#---------------------------Security Group ----------------------------------------
+
+resource "aws_security_group" "sg_webserver" {
+  name   = "Allowing http and SSH access from internet"
+  vpc_id = aws_vpc.my_vpc_01.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
